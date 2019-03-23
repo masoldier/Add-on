e8d9305782fcdfb44bee399324ea19a375f5c2e1
Rkcreativeworks <35627765+Rkcreativeworks@users.noreply.github.com>
2019-03-23 12:30:39 +0530

    Update ultrasonic.ino

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,5 +1,3 @@
-#include<LiquidCrystal.h>
-LiquidCrystal lcd(13,12,8,7,4,2);
 const unsigned int TRIG_PIN=5;
 const unsigned int ECHO_PIN=6;
 const unsigned int BAUD_RATE=9600;
@@ -9,7 +7,7 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   Serial.begin(BAUD_RATE);
-  lcd.begin(16,2);
+  
 }
 
 void loop() {
@@ -24,20 +22,14 @@ void loop() {
  int distance= duration/29/2;
  if(duration==0){
    Serial.println("Warning: no pulse from sensor");
-   lcd.clear();
-   lcd.print("WARNING");
+   
    } 
   else{
-      lcd.setCursor(0,0);
-      lcd.clear();
-      lcd.print("Distance");
+     
       Serial.print("distance to nearest object:");
       Serial.println(distance);
       Serial.println(" cm");
-      lcd.setCursor(0,1);
-      lcd.print(distance);
-      lcd.print("cm");
-      lcd.setCursor(0,0);
+     
   }
  delay(100);
  }
